ccb352f79b509d428811a72afb04a5d6f9235792
diptomistry <diptomistrybd@gmail.com>
2023-10-27 21:38:13 +0600

    aa

--- cProgramming/loops/commonAlphaDynamic.c
+++ cProgramming/loops/commonAlphaDynamic.c
@@ -1,84 +1,58 @@
 #include <stdio.h>
 
-#define MAX_STRINGS 10000
-#define MAX_STRING_LENGTH 10001
+const int MAX_CHAR = 52;  
 
-void findCommonAlphabets(char strings[MAX_STRINGS][MAX_STRING_LENGTH], int numStrings) {
-    if (numStrings <= 0) {
-        printf("###\n"); // No common alphabets (no input strings)
-        return;
+void commonCharacters(char str[][10000], int n) {
+   
+    int common[MAX_CHAR];
+    for (int i = 0; i < MAX_CHAR; i++) {
+        common[i] = 1;
     }
 
-    char commonAlphabets[26];
-    int commonCount = 0;
+ 
+    for (int i = 0; i < n; i++) {
+      
+        int current[MAX_CHAR] = {0};
 
-    char *firstString = strings[0];
-
-    for (int i = 0; firstString[i] != '\0'; i++) {
-        char c = firstString[i];
-        int isCommon = 1;
-
-        for (int j = 1; j < numStrings; j++) {
-            char *currentString = strings[j];
-            int found = 0;
-            for (int k = 0; currentString[k] != '\0'; k++) {
-                if (currentString[k] == c) {
-                    found = 1;
-                    break;
-                }
-            }
-            if (!found) {
-                isCommon = 0;
-                break;
+        for (int j = 0; str[i][j]; j++) {
+            char c = str[i][j];
+            if (c >= 'a' && c <= 'z') {
+                current[c - 'a'] = 1;
+            } else if (c >= 'A' && c <= 'Z') {
+                current[c - 'A' + 26] = 1;
             }
         }
 
-        if (isCommon) {
-            commonAlphabets[commonCount] = c;
-            commonCount++;
+       
+        for (int j = 0; j < MAX_CHAR; j++) {
+            common[j] &= current[j];
         }
     }
 
-    if (commonCount == 0) {
-        printf("###\n"); // No common alphabets found
-        return;
-    }
-
-    // Sort commonAlphabets array in alphabetical order
-    for (int i = 0; i < commonCount - 1; i++) {
-        for (int j = i + 1; j < commonCount; j++) {
-            if (commonAlphabets[i] > commonAlphabets[j]) {
-                // Bubble sort
-                char temp = commonAlphabets[i];
-                commonAlphabets[i] = commonAlphabets[j];
-                commonAlphabets[j] = temp;
+    for (int i = 0; i < MAX_CHAR; i++) {
+        if (common[i]) {
+            if (i < 26) {
+                printf("%c", i + 'a');
+            } else {
+                printf("%c", i + 'A' - 26);
             }
         }
-    }
-
-    for (int i = 0; i < commonCount; i++) {
-        printf("%c", commonAlphabets[i]);
     }
     printf("\n");
 }
 
 int main() {
-    int numStrings;
+    int n;
+    
+    scanf("%d", &n);
 
-    scanf("%d", &numStrings);
-    getchar(); // Consume the newline character
+    char str[n][10000];
 
-    char strings[MAX_STRINGS][MAX_STRING_LENGTH];
-
-    for (int i = 0; i < numStrings; i++) {
-        if (scanf("%s", strings[i]) == EOF) {
-            // Handle input error
-            printf("Input error.\n");
-            return 1;
-        }
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%s", str[i]);
     }
 
-    findCommonAlphabets(strings, numStrings);
-
+    commonCharacters(str, n);
     return 0;
 }
