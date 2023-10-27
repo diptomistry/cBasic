cf1002ad1403ac39386b870fcbfef2f607bbaa9b
diptomistry <diptomistrybd@gmail.com>
2023-10-27 20:18:28 +0600

    aa

--- cProgramming/loops/commonAlphaDynamic.c
+++ cProgramming/loops/commonAlphaDynamic.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
-#include <string.h> 
+#include <stdlib.h>
 
-void findCommonAlphabets(char strings[][100], int numStrings) {
+void findCommonAlphabets(char **strings, int numStrings) {
     if (numStrings <= 0) {
-        printf("###\n"); 
+        printf("###\n"); // No common alphabets (no input strings)
         return;
     }
 
     char commonAlphabets[26];
     int commonCount = 0;
 
-    char firstString[100];
-    strcpy(firstString, strings[0]);
+    char *firstString = strings[0];
 
     for (int i = 0; firstString[i] != '\0'; i++) {
         char c = firstString[i];
         int isCommon = 1;
 
         for (int j = 1; j < numStrings; j++) {
-            if (strchr(strings[j], c) == NULL) {
+            char *currentString = strings[j];
+            int found = 0;
+            for (int k = 0; currentString[k] != '\0'; k++) {
+                if (currentString[k] == c) {
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found) {
                 isCommon = 0;
                 break;
             }
@@ -35,11 +42,11 @@ void findCommonAlphabets(char strings[][100], int numStrings) {
         return;
     }
 
-  
+    
     for (int i = 0; i < commonCount - 1; i++) {
         for (int j = i + 1; j < commonCount; j++) {
             if (commonAlphabets[i] > commonAlphabets[j]) {
-            
+                // Bubble sort
                 char temp = commonAlphabets[i];
                 commonAlphabets[i] = commonAlphabets[j];
                 commonAlphabets[j] = temp;
@@ -57,15 +64,21 @@ int main() {
     int numStrings;
 
     scanf("%d", &numStrings);
-    getchar(); 
+    getchar(); // Consume the newline character
 
-    char strings[numStrings][100];
-
+    char **strings = (char **)malloc(numStrings * sizeof(char *));
     for (int i = 0; i < numStrings; i++) {
+        strings[i] = (char *)malloc(10001 * sizeof(char)); // Maximum string length is 10,000
         scanf("%s", strings[i]);
     }
 
     findCommonAlphabets(strings, numStrings);
+
+    // Free dynamically allocated memory
+    for (int i = 0; i < numStrings; i++) {
+        free(strings[i]);
+    }
+    free(strings);
 
     return 0;
 }
