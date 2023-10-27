9fe9557c650f5074c18af93e5e106f4bcd14b16a
diptomistry <diptomistrybd@gmail.com>
2023-10-27 21:42:18 +0600

    sfg

--- cProgramming/loops/commonAlphaDynamic.c
+++ cProgramming/loops/commonAlphaDynamic.c
@@ -1,55 +1,85 @@
 #include <stdio.h>
 
-const int MAX_CHAR = 52;  
+const int MAX_CHAR = 52; // 26 for lowercase and 26 for uppercase
 
-void commonCharacters(char str[][10000], int n) {
-   
+void commonCharacters(char str[][100], int n)
+{
+
     int common[MAX_CHAR];
-    for (int i = 0; i < MAX_CHAR; i++) {
+    for (int i = 0; i < MAX_CHAR; i++)
+    {
         common[i] = 1;
     }
 
- 
-    for (int i = 0; i < n; i++) {
-      
+    for (int i = 0; i < n; i++)
+    {
+
         int current[MAX_CHAR] = {0};
 
-        for (int j = 0; str[i][j]; j++) {
+        for (int j = 0; str[i][j]; j++)
+        {
             char c = str[i][j];
-            if (c >= 'a' && c <= 'z') {
+            if (c >= 'a' && c <= 'z')
+            {
                 current[c - 'a'] = 1;
-            } else if (c >= 'A' && c <= 'Z') {
+            }
+            else if (c >= 'A' && c <= 'Z')
+            {
                 current[c - 'A' + 26] = 1;
             }
         }
 
-       
-        for (int j = 0; j < MAX_CHAR; j++) {
+        for (int j = 0; j < MAX_CHAR; j++)
+        {
             common[j] &= current[j];
         }
     }
 
-    for (int i = 0; i < MAX_CHAR; i++) {
-        if (common[i]) {
-            if (i < 26) {
-                printf("%c", i + 'a');
-            } else {
-                printf("%c", i + 'A' - 26);
+    int foundCommon = 0;
+    for (int i = 0; i < MAX_CHAR; i++)
+    {
+        if (common[i])
+        {
+            foundCommon = 1;
+            break;
+        }
+    }
+
+    if (foundCommon)
+    {
+
+        for (int i = 0; i < MAX_CHAR; i++)
+        {
+            if (common[i])
+            {
+                if (i < 26)
+                {
+                    printf("%c", i + 'a');
+                }
+                else
+                {
+                    printf("%c", i + 'A' - 26);
+                }
             }
         }
+    }
+    else
+    {
+        printf("###");
     }
     printf("\n");
 }
 
-int main() {
+int main()
+{
     int n;
-    
+
     scanf("%d", &n);
 
-    char str[n][10000];
+    char str[n][100];
 
-    
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         scanf("%s", str[i]);
     }
 
